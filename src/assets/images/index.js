96fd6a95a559800367a359b9e4f4c7b0dcc26115
Phạm Văn Huy <huypv@riofintech.vn>
2023-11-11 14:43:58 +0700

    feat: 3d portfolio

---
+++ src/assets/images/index.js
@@ -0,0 +1,17 @@
+import hero from './hero.jpg'
+import meta from './meta.png'
+import shopify from './shopify.png'
+import rio from './rio_fintech_logo.jpg'
+import starbucks from './starbucks.png'
+import tesla from './tesla.png'
+import logo from './logo.svg'
+
+export {
+    hero,
+    meta,
+    shopify,
+    starbucks,
+    tesla,
+    logo,
+    rio
+}
